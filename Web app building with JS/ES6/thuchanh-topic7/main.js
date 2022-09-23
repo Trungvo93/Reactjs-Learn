9bff96abdd600d63467fed51d26bb5ded0e11a7f
Trungvo93 <thanhtrungvo1993@gmail.com>
2022-09-23 15:05:22 +0700

    .

---
+++ Web app building with JS/ES6/thuchanh-topic7/main.js
@@ -0,0 +1,16 @@
+// export let greet = "Xin chào CODEGYM ONLINE!";
+// export const LESSON = "Sử dụng Module trong ES6";
+ 
+// export function greeting(name, content) {
+//     return name + " " +content;
+// }
+ 
+let greet = "Xin chào CODEGYM ONLINE!";
+const LESSON = "Sử dụng Module trong ES6";
+ 
+function greeting(name, content) {
+    return name + " " +content;
+}
+ 
+// Xuất các biến và hàm
+export { greet, LESSON, greeting };
